3871d9ddb5770e5b20009ef1955f28db0faabc7a
kudzanai murefu <kudzmurefu@gmail.com>
2023-06-24 16:41:46 -0400

    used env for screts

--- processAudio/speachToTextHandler.js
+++ processAudio/speachToTextHandler.js
@@ -1,19 +1,12 @@
 import speech from '@google-cloud/speech'
 import {config} from 'dotenv'
 import fs from 'fs'
-import gcpScrets from '@google-cloud/secret-manager'
-
 config()
 
 export const speechToTextHandler = async () => {
-  const speechCredentialKey = process.env.ENVIROMENT === 'dev' ? JSON.parse(process?.env?.SA_KEY) :  await((async()=>{
-    const [secret] = await (new gcpScrets.v1.SecretManagerServiceClient().getSecret({ name: 'speechCredentialKey'}))
-    console.log(`Log - gcPsecret :: speechToTextHandler :: ${JSON.stringify(secret)}`)
-    return secret
-  }))()
-  console.log(`Log - speechCredentialKey :: speechToTextHandler :: ${JSON.stringify(speechCredentialKey)}`)
+  console.log(`Log - speechCredentialKey :: speechToTextHandler :: Entry`)
   try {
-    const client = new speech.SpeechClient({credentials: speechCredentialKey})
+    const client = new speech.SpeechClient({credentials: JSON.parse(process?.env?.SA_KEY)})
     const [response] = await client.recognize({
       config: {
         sampleRateHertz: 48000,
@@ -22,6 +15,8 @@ export const speechToTextHandler = async () => {
       },
       audio: {content: fs.readFileSync('uploads/voiceRecording.wav').toString('base64')}
     })
+    console.log(`Log - speechCredentialKey :: 
+      speechToTextHandler :: ${response?.results?.map(result => result?.alternatives[0]?.transcript)?.join('\n')}`)
     return response.results.map(result => result?.alternatives[0]?.transcript)?.join('\n')
   } catch(e){
     console.log(`Error :: speechToTextHandler :: ${e.message}`)
